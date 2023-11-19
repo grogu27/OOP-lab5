9cb327ed3b608a3ba009a8c7ec82351e8ac53275
Soskin Andrey <arthas@-.localdomain>
2023-11-19 19:08:23 +0700

    31231

--- my_class.hpp
+++ my_class.hpp
@@ -6,15 +6,14 @@ class Figure
 protected:
     int x;
     int y;
+
+    void Set_val(int x, int y);
 public:
     Figure();
   
     Figure(int x, int y);
   
     void Get_val();
-  
-    void Set_val(int x, int y);
- 
 };
 
 class Line: public Figure
@@ -23,18 +22,14 @@ protected:
     double len;
     int x2;
     int y2;
+    void Get_line_len();
+    void Set_val(int x2, int y2);
 public:
     Line();
   
     Line(int x, int y, int x2, int y2);
  
-    void Get_line_len();
- 
     void Get_val();
-    
-    void Set_val(int x2, int y2);
-
-    
 };
 
 class Square : public Line
@@ -42,19 +37,16 @@ class Square : public Line
 protected:
     double perimeter;
     double area;
+
+    void Set_val();
+    void Get_perimeter();
+    void Get_area();
 public:
     Square();
   
     Square(int x, int y, int x2, int y2);
-   
-    void Get_perimeter();
-  
-    void Get_area();
-  
-    void Get_val();
 
-    void Set_val();
- 
+    void Get_val();
 };
 class Rectangle : public Square
 {
@@ -63,16 +55,16 @@ protected:
     int y3;
     int x4;
     int y4;
-public:
-    Rectangle();
- 
-    Rectangle(int x, int y, int x2, int y2);
 
     void Get_area();
  
     void Get_perimeter();
  
     void Set_val();
+public:
+    Rectangle();
+ 
+    Rectangle(int x, int y, int x2, int y2);
 
     void Get_val();
 
@@ -82,29 +74,25 @@ class Circle : public Figure
 {
 protected:
     double radius;
+
+    void Set_val(double radius);
 public:
     Circle();
  
     Circle(int x, int y, double radius);
  
     void Get_val();
-  
-    void Set_val(double radius);
-  
-
 };
 
 class Ellipse : public Circle {
 protected:
-double rx, ry;
+    double rx, ry;
+
+    void Set_val(double rx, double ry);
 public:
     Ellipse();
  
     Ellipse(int x, int y, double rx, double ry);
   
     void Get_val();
-   
-    void Set_val(double rx, double ry);
- 
-
 };
